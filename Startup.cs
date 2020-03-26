e9d8b89d507d99c5cc4184ae3ecb4fac1dff8851
singhutkarsh304 <singhutkarsh304@gmail.com>
2020-03-26 12:08:02 +0530

    localhost changes

--- Startup.cs
+++ Startup.cs
@@ -32,11 +32,16 @@ namespace SoftwareDownloadSystem
             {
                 configuration.RootPath = "ClientApp/build";
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            options.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
